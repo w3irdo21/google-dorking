6a4b149f53194f2e69ad563ce9645c97430b47d3
Sle3pyHead <101105827+w3irdo21@users.noreply.github.com>
2025-10-08 11:46:36 +0530

    simplified for ease

--- dorkbuilder.py
+++ dorkbuilder.py
@@ -1,21 +1,20 @@
-def create_dorks(filename, batch_size=10):
+def create_dorks(filename, batch_size=6, suffix='intitle:"Swagger UI"'):
     with open(filename, "r") as f:
         domains = [line.strip().lstrip("*.") for line in f if line.strip()]
 
     dorks = []
     for i in range(0, len(domains), batch_size):
         batch = domains[i:i + batch_size]
-        dork = "(" + " OR ".join(f"site:{d}" for d in batch) + ") inurl:\"/web/guest/home\""    //this was for CVE-2025-4388, change this last for others
+        dork = "(" + " OR ".join(f"site:{d}" for d in batch) + f") {suffix}"
         dorks.append(dork)
 
     return dorks
 
 
 if __name__ == "__main__":
-    dorks = create_dorks("domains.txt", batch_size=10)
+    dorks = create_dorks("domains.txt", batch_size=6, suffix='intitle:"Swagger UI"')
     with open("dorks.txt", "w") as f:
         for dork in dorks:
             f.write(dork + "\n")
 
     print(f"Generated {len(dorks)} dork lines. Saved to dorks.txt")
-
